3e52d396f1186f0ce76b130517b1b5f49e883741
ahmomoz <x5426854268@gmail.com>
2025-04-28 12:48:55 +0800

    feat: 增加部落格內文的網頁標題

--- components/Blog/Article/Content.vue
+++ components/Blog/Article/Content.vue
@@ -1,4 +1,5 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+</script>
 
 <template>
   <main class="blog-articleContent-section border-top-bottom wrap">
@@ -9,8 +10,7 @@
           <div class="blog-mainArticle-wrap f-column gap-6">
             <BlogArticleSection :title="'前言'">
               <p class="mt-6">
-                嗨，我是
-                Alyse，一名前端工程師兼職涯諮詢師。一直以來，我都很喜歡在部落格分享學習與工作心得，也常有讀者問：「我想轉職/自學前端，該從哪裡開始？」
+                嗨，我是 Alyse，一名前端工程師兼職涯諮詢師。一直以來，我都很喜歡在部落格分享學習與工作心得，也常有讀者問：「我想轉職/自學前端，該從哪裡開始？」
                 <br />
                 其實自學的過程既自由又具挑戰性。我整理了三大關鍵，幫助你在短期內建立紮實基礎，並快速累積實戰經驗。希望能替你的前端之路帶來一些啟發與動力！
               </p>
--- components/Blog/Article/Cover.vue
+++ components/Blog/Article/Cover.vue
@@ -1,4 +1,10 @@
 <script lang="ts" setup>
+useSeoMeta({
+  title: "自學前端不用怕：從零開始的三大關鍵",
+  description: " 嗨，我是 Alyse，一名前端工程師兼職涯諮詢師。一直以來，我都很喜歡在部落格分享學習與工作心得，也常有讀者問：「我想轉職/自學前端，該從哪裡開始？」其實自學的過程既自由又具挑戰性。我整理了三大關鍵，幫助你在短期內建立紮實基礎，並快速累積實戰經驗。希望能替你的前端之路帶來一些啟發與動力！",
+  ogTitle: "自學前端不用怕：從零開始的三大關鍵",
+  ogDescription: " 嗨，我是 Alyse，一名前端工程師兼職涯諮詢師。一直以來，我都很喜歡在部落格分享學習與工作心得，也常有讀者問：「我想轉職/自學前端，該從哪裡開始？」其實自學的過程既自由又具挑戰性。我整理了三大關鍵，幫助你在短期內建立紮實基礎，並快速累積實戰經驗。希望能替你的前端之路帶來一些啟發與動力！",
+});
 </script>
 
 <template>
@@ -15,10 +21,10 @@
                 </p>
               </time>
               <div class="article-tag-wrap f-align-center mt-1">
-                <h1 class="fs-4 fw-medium text-primary">前端開發 x 職涯成長</h1>
+                <h3 class="fs-4 fw-medium text-primary">前端開發 x 職涯成長</h3>
                 <span class="tag-primary ms-2">最新文章</span>
               </div>
-              <h3 class="card-title">自學前端不用怕：從零開始的三大關鍵</h3>
+              <h1 class="card-title fs-3">自學前端不用怕：從零開始的三大關鍵</h1>
             </div>
           </article>
         </div>
